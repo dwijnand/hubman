c029128c1549526bb8c052299a0412abba60fa86
Dale Wijnand <dale.wijnand@gmail.com>
2019-01-03 11:08:59 +0000

    print fork branches

---
+++ src/bin/print-my-empty-forks.rs
@@ -0,0 +1,49 @@
+#![allow(dead_code)]
+#![allow(non_upper_case_globals)]
+#![allow(unused_assignments)]
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+
+use std::env;
+
+use futures::{Future, Stream};
+use hyper::Client;
+use hyper_tls::HttpsConnector;
+use tokio::runtime::Runtime;
+use hubcaps::{Credentials, Error, Github, HttpCache, Result};
+use hubcaps::repositories::*;
+
+fn main() -> Result<()> {
+    pretty_env_logger::init();
+
+    let mut rt = Runtime::new()?;
+
+    let host = "https://api.github.com";
+    let agent = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"));
+    let credentials = env::var("GITHUB_TOKEN").ok().map(Credentials::Token);
+    let client = Client::builder().build(HttpsConnector::new(4).unwrap());
+    let http_cache = HttpCache::in_home_dir();
+    let github: Github<_> = Github::custom(host, agent, credentials, client, http_cache);
+
+    rt.block_on(github.repos().iter(&RepoListOptions::builder().affiliation(vec![Affiliation::Owner]).build()).filter(|r| r.fork).fold((), move |(), r| {
+        println!();
+        print!("https://github.com/{}/branches: ", r.full_name);
+        let credentials = env::var("GITHUB_TOKEN").ok().map(Credentials::Token);
+        let client = Client::builder().build(HttpsConnector::new(4).unwrap());
+        let http_cache = HttpCache::in_home_dir();
+        let github: Github<_> = Github::custom(host, agent, credentials, client, http_cache);
+        let branches_stream = github.repo(r.owner.login, r.name).branches().iter();
+        fn per_branch(b: hubcaps::branches::Branch) {
+            let protected = match b.protected {
+                Some(true) => " (protected)",
+                Some(false) => "",
+                None => ""
+            };
+            print!("{}{}, ", b.name, protected);
+        }
+        branches_stream.fold((), |(), b| {
+            per_branch(b);
+            futures::future::ok::<(), Error>(())
+        })
+    }))
+}
