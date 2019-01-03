da038603487c42a3e49cffbf45403246d4a2ddfb
Dale Wijnand <dale.wijnand@gmail.com>
2019-01-03 12:17:36 +0000

    Add AsyncRun

--- src/bin/print-my-empty-forks.rs
+++ src/bin/print-my-empty-forks.rs
@@ -7,16 +7,14 @@
 use std::env;
 
 use futures::{Future, Stream};
+use hubcaps::branches::Branch;
+use hubcaps::repositories::*;
+use hubcaps::{Credentials, Error, Github, HttpCache, Result};
 use hyper::Client;
 use hyper_tls::HttpsConnector;
-use tokio::runtime::Runtime;
-use hubcaps::{Credentials, Error, Github, HttpCache, Result};
-use hubcaps::repositories::*;
 
 fn main() -> Result<()> {
     pretty_env_logger::init();
-
-    let mut rt = Runtime::new()?;
 
     let host = "https://api.github.com";
     let agent = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"));
@@ -25,15 +23,23 @@ fn main() -> Result<()> {
     let http_cache = HttpCache::in_home_dir();
     let github: Github<_> = Github::custom(host, agent, credentials, client, http_cache);
 
-    rt.block_on(github.repos().iter(&RepoListOptions::builder().affiliation(vec![Affiliation::Owner]).build()).filter(|r| r.fork).fold((), move |(), r| {
-        println!();
+    let my_forks = github
+        .repos()
+        .iter(
+            &RepoListOptions::builder()
+                .affiliation(vec![Affiliation::Owner])
+                .build(),
+        )
+        .filter(|r| r.fork);
+
+    my_forks.fold((), move |(), r| {
         print!("https://github.com/{}/branches: ", r.full_name);
+
         let credentials = env::var("GITHUB_TOKEN").ok().map(Credentials::Token);
         let client = Client::builder().build(HttpsConnector::new(4).unwrap());
         let http_cache = HttpCache::in_home_dir();
         let github: Github<_> = Github::custom(host, agent, credentials, client, http_cache);
-        let branches_stream = github.repo(r.owner.login, r.name).branches().iter();
-        fn per_branch(b: hubcaps::branches::Branch) {
+        fn per_branch(b: Branch) {
             let protected = match b.protected {
                 Some(true) => " (protected)",
                 Some(false) => "",
@@ -41,9 +47,31 @@ fn main() -> Result<()> {
             };
             print!("{}{}, ", b.name, protected);
         }
-        branches_stream.fold((), |(), b| {
-            per_branch(b);
-            futures::future::ok::<(), Error>(())
-        })
-    }))
+        github.repo(r.owner.login, r.name).branches().iter()
+            .fold((), |(), b| {
+                per_branch(b);
+                futures::future::ok::<(), Error>(())
+            })
+            .and_then(|()| {
+                println!();
+                futures::future::ok::<(), Error>(())
+            })
+    }).run();
+
+    Ok(())
+}
+
+trait AsyncRun {
+    fn run(self);
+}
+
+impl<F> AsyncRun for F where
+    F: Future<Item = (), Error = Error> + Send + 'static
+{
+    fn run(self) {
+        tokio::run(self.map_err(|err| {
+            eprintln!("Error: {}", err);
+            ()
+        }))
+    }
 }
