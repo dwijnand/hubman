e45ab062af545d135b6d512ece7ad448c715c884
Dale Wijnand <dale.wijnand@gmail.com>
2019-01-03 13:50:54 +0000

    Extract github function

--- src/bin/print-my-empty-forks.rs
+++ src/bin/print-my-empty-forks.rs
@@ -12,22 +12,25 @@ use futures::{future, stream};
 use hubcaps::branches::Branch;
 use hubcaps::repositories::*;
 use hubcaps::{Credentials, Error, Github, HttpCache, Result};
+use hyper::client::connect::Connect;
 use hyper::Client;
 use hyper_tls::HttpsConnector;
 use tokio::runtime::Runtime;
 
-fn main() -> Result<()> {
-    pretty_env_logger::init();
-    let ref mut rt = Runtime::new()?;
-
+fn github() -> Github<impl Clone + Connect + 'static> {
     let host = "https://api.github.com";
     let agent = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"));
     let credentials = env::var("GITHUB_TOKEN").ok().map(Credentials::Token);
     let client = Client::builder().build(HttpsConnector::new(4).unwrap());
     let http_cache = HttpCache::in_home_dir();
-    let github: Github<_> = Github::custom(host, agent, credentials, client, http_cache);
+    Github::custom(host, agent, credentials, client, http_cache)
+}
 
-    let my_forks = github
+fn main() -> Result<()> {
+    pretty_env_logger::init();
+    let ref mut rt = Runtime::new()?;
+
+    let my_forks = github()
         .repos()
         .iter(
             &RepoListOptions::builder()
@@ -37,36 +40,43 @@ fn main() -> Result<()> {
         .filter(|r| r.fork);
 
     let my_forks_with_branches = my_forks.and_then(move |r| {
-        let credentials = env::var("GITHUB_TOKEN").ok().map(Credentials::Token);
-        let client = Client::builder().build(HttpsConnector::new(4).unwrap());
-        let http_cache = HttpCache::in_home_dir();
-        let github: Github<_> = Github::custom(host, agent, credentials, client, http_cache);
-        let branches = github.repo(r.owner.login.deref(), r.name.deref()).branches().iter();
+        let branches = github()
+            .repo(r.owner.login.deref(), r.name.deref())
+            .branches()
+            .iter();
         branches.collect().map(move |branches| (r, branches))
     });
 
-    my_forks_with_branches.for_each(|(r, bs)| {
-        fn is_protected(b: &Branch) -> bool { b.protected == Some(true) }
-        fn per_branch(b: &Branch) {
-            let protected = if is_protected(b) { " (protected)" } else { "" };
-            print!("{}{}, ", b.name, protected);
-        };
-        if bs.len() == 1 {
-            let b = &bs[0];
-            if !is_protected(&b) && ["master", "z"].iter().any(|n| n == &b.name) {
-                println!("DELETE https://github.com/{}/branches: {}", r.full_name, b.name);
+    my_forks_with_branches
+        .for_each(move |(r, bs)| {
+            fn is_protected(b: &Branch) -> bool {
+                b.protected == Some(true)
             }
-        }
-        Ok(())
-    }).run(rt)
+            fn per_branch(b: &Branch) {
+                let protected = if is_protected(b) { " (protected)" } else { "" };
+                print!("{}{}, ", b.name, protected);
+            };
+            if bs.len() == 1 {
+                let b = &bs[0];
+                if !is_protected(&b) && ["master", "z"].iter().any(|n| n == &b.name) {
+                    println!(
+                        "DELETE https://github.com/{}/branches: {}",
+                        r.full_name, b.name
+                    );
+                }
+            }
+            Ok(())
+        })
+        .run(rt)
 }
 
 trait AsyncRun {
     fn run(self, rt: &mut Runtime) -> Result<()>;
 }
 
-impl<F> AsyncRun for F where
-    F: Future<Item = (), Error = Error> + Send + 'static
+impl<F> AsyncRun for F
+where
+    F: Future<Item = (), Error = Error> + Send + 'static,
 {
     fn run(self, rt: &mut Runtime) -> Result<()> {
         rt.block_on(self)
